9033f9b0b5b1757f152b8e46074f5676b1b42d6d
github username <sjami002@gmail.com>
2019-11-04 23:59:05 -0500

    movie components, css

--- movie-db/src/Movie/Movie.js
+++ movie-db/src/Movie/Movie.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { API_URL, API_KEY } from "../config";
+import Navigation from "../elements/Navigation/Navigation";
+import MovieInfo from "../elements/MovieInfo/MovieInfo";
+import MovieInfoBar from "../elements/MovieInfoBar/MovieInfoBar";
+import FourColGrid from "../elements/FourColGrid/FourColGrid.js";
+import Actor from "../elements/Actor/Actor";
+import Spinner from "../elements/Spinner/Spinner";
 
 class Movie extends Component {
   state = {
