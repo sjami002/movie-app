f3121b0e4c0c1e7c16e2523e5a3ae6b732249360
github username <sjami002@gmail.com>
2019-11-04 23:20:33 -0500

    routers, links, app, header

--- movie-db/src/App.js
+++ movie-db/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./elements/Header/Header";
 import Homepage from "./Homepage/Homepage";
+import Movie from "./Movie/Movie";
+import NotFound from "./elements/NotFound/NotFound";
 
-const App = () => {
-  return (
-    <div>
+const App = () => (
+  <BrowserRouter>
+    <React.Fragment>
       <Header />
-      <Homepage />
-    </div>
-  );
-};
+      <Switch>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/:movieId" component={Movie} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </React.Fragment>
+  </BrowserRouter>
+);
 
 export default App;
--- movie-db/src/elements/MovieThumb/MovieThumb.js
+++ movie-db/src/elements/MovieThumb/MovieThumb.js
@@ -1,11 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./MovieThumb.css";
 
-const MovieThumb = props => {
-  return (
-    <div className="rmdb-moviethumb">
-      <img src={props.image} alt="moviethumb" />{" "}
-    </div>
-  );
+const MovieThumb = ({ image, movieId, movieName, clickable }) => (
+  <div className="rmdb-moviethumb">
+    {/* You can send props via the Links "to" object. Here we create our own "movieName" */}
+    {clickable ? (
+      <Link to={{ pathname: `/${movieId}`, movieName: `${movieName}` }}>
+        <img className="clickable" src={image} alt="moviethumb" />
+      </Link>
+    ) : (
+      <img src={image} alt="moviethumb" />
+    )}
+  </div>
+);
+
+MovieThumb.propTypes = {
+  image: PropTypes.string,
+  movieId: PropTypes.number,
+  movieName: PropTypes.string,
+  clickable: PropTypes.bool
 };
 
 export default MovieThumb;
